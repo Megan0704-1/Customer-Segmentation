1b6335b35e78c026d596b88c3fefaf13a9cbc0f6
megankuo <megankuo@asu.edu>
2025-02-01 13:05:31 -0700

    remove cached models:

--- part1/part1.py
+++ part1/part1.py
@@ -95,12 +95,14 @@ class SimpleMLP(nn.Module):
         self.fc1 = nn.Linear(784, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 10)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        logits = F.relu(self.fc3(x))
+        x = F.relu(self.fc3(x))
+        logits = self.softmax(x)
 
         return logits
 
